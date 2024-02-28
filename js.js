99f0f45da9612f590cc2baf8f5722f425f96f070
daskzpos <dashapostma@gmail.com>
2024-02-28 11:37:19 +0100

    Initial commit

---
+++ js.js
@@ -0,0 +1,8 @@
+const names = [];
+
+function addImage(){
+    var img = document.createElement("img");
+    img.src = "https://www.the-sun.com/wp-content/uploads/sites/6/2023/10/KS-crunchy-cat-comp.jpg?w=620";
+var src = document.getElementById("img");
+src.appendChild(img);
+}
